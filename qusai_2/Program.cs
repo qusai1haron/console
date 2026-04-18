2d94246276f859fae1b9fb0d07d4c8fd7ec3a147
qusai haron <ahqsy66@gmail.com>
2026-04-18 18:29:33 +0300

    Assimant_1

--- qusai_2/Program.cs
+++ qusai_2/Program.cs
@@ -1,6 +1,24 @@
-﻿Console.WriteLine("string");
+﻿Console.WriteLine("Entar the invalid amount");
 
-if(gread > 50)
+double amount = Convert.ToDouble (Console.ReadLine());
+
+if (amount <= 0)
 {
-    Console.WriteLine(
+    Console.WriteLine($"invalid amount : {amount}");
+}
+else if(amount >= 500)
+{
+    double discountAmount = (amount * 0.2);
+    double finalAmount = amount - discountAmount;
+    Console.WriteLine($"the final amount after discount (20%) : {finalAmount}");
+}
+else if(amount <500 && amount >= 300)
+{
+    double discountAmount = (amount * 0.1);
+    double finalAmount = amount - discountAmount;
+    Console.WriteLine($"the final amount after discount (10%) : {finalAmount}");
+}
+else
+{
+    Console.WriteLine($"the final amount : {amount}");
 }
